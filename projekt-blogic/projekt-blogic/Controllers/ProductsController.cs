3d5101312e5c9eb8b5823d12bce45d279320e39e
LAPTOP-RTBVRDRR\adgar <ad.gargulak@gmail.com>
2025-05-14 10:03:14 +0200

    projekt-zmena

--- projekt-blogic/projekt-blogic/Controllers/ProductsController.cs
+++ projekt-blogic/projekt-blogic/Controllers/ProductsController.cs
@@ -9,11 +9,61 @@ namespace projekt_blogic.Controllers
 
         public IActionResult Administration()
         {
-            var vm = new AdministrationViewModel
+            var vm = new AdminViewModel
             {
-                vm.Products = _products
-            }
+                Products = _products
+            };
             return View(vm);
         }
+
+        public IActionResult Insert([FromBody] Product newProduct)
+        {
+            if (newProduct != null)
+            {
+                newProduct.Id = _products.Max(p => p.Id) + 1;
+                _products.Add(newProduct);
+                return Ok(newProduct);
+            }
+
+            return BadRequest();
+        }
+
+        public IActionResult Edit([FromBody] Product updatedProduct)
+        {
+            var existing = _products.FirstOrDefault(p => p.Id == updatedProduct.Id);
+            if (existing != null)
+            {
+                existing.Name = updatedProduct.Name;
+                existing.Price = updatedProduct.Price;
+                return Ok(existing);
+            }
+
+            return NotFound();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                _products.Remove(product);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class AdminViewModel
+    {
+        public List<Product> Products { get; set; }
     }
 }
+
