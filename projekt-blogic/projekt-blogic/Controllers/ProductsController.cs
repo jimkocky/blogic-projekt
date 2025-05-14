9dd47d48b928a47266a7a76726829d30fb89ca57
LAPTOP-RTBVRDRR\adgar <ad.gargulak@gmail.com>
2025-05-14 09:02:30 +0200

    databaze

---
+++ projekt-blogic/projekt-blogic/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projekt_blogic.Models.Products;
+
+namespace projekt_blogic.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static List<Product> _products = new List<Product> { new Product { Name = "Pepsi", Price = 42 } };
+
+        public IActionResult Administration()
+        {
+            var vm = new AdministrationViewModel
+            {
+                vm.Products = _products
+            }
+            return View(vm);
+        }
+    }
+}
---
+++ projekt-blogic/projekt-blogic/Views/Products/AdministrationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using projekt_blogic.Models.Products;
+
+namespace projekt_blogic.Views.Products  
+
+{
+    public class AdministrationViewModel
+    {
+        public IList<Product> Products { get; set; }
+    }
+}
