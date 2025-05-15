4d09c75563cc58207590297d1de7f497dd971f3c
LAPTOP-RTBVRDRR\adgar <ad.gargulak@gmail.com>
2025-05-15 14:22:50 +0200

    database-ProcProducts

---
+++ projekt-blogic/projekt-blogic.DB/ProcProductDelete.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE ProcProductDelete
+    @ProductId INT
+AS
+BEGIN
+    UPDATE Products
+    SET 
+        IsDeleted = 1,
+        DateUpdated = GETDATE()
+    WHERE ProductId = @ProductId AND IsDeleted = 0;
+END;
