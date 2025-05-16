6449e7a9dd33fa361107acb2626acbd41845498a
LAPTOP-RTBVRDRR\adgar <ad.gargulak@gmail.com>
2025-05-16 14:35:14 +0200

    ProductsController-update

--- projekt-blogic/projekt-blogic.DB/Tables/Products.sql
+++ projekt-blogic/projekt-blogic.DB/Tables/Products.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[Products]
 (
-	[ProductID] INT NOT NULL PRIMARY KEY, 
-    [DateCreated] DATETIME NULL DEFAULT GETDATE(), 
-    [CreatedBy] INT NULL DEFAULT 0, 
-    [DateUpdated] DATETIME NULL DEFAULT GETDATE(), 
-    [UpdatedBy] INT NULL DEFAULT 0, 
-    [IsDeleted] BIT NULL DEFAULT 0, 
-    [Name] NVARCHAR(512) NULL, 
-    [Price] INT NULL, 
-    [Quantity] INT NULL, 
-    [ImageURL] NVARCHAR(1024) NULL
+	[ProductID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [DateCreated] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [CreatedBy] INT NOT NULL DEFAULT 0, 
+    [DateUpdated] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [UpdatedBy] INT NOT NULL DEFAULT 0, 
+    [IsDeleted] BIT NOT NULL DEFAULT 0, 
+    [Name] NVARCHAR(512) NOT NULL, 
+    [Price] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [ImageURL] NVARCHAR(1024) NOT NULL
 )
--- projekt-blogic/projekt-blogic/ViewModels/AdministrationViewModel.cs
+++ projekt-blogic/projekt-blogic/ViewModels/AdministrationViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using projekt_blogic.Models.Products;
 
 namespace projekt_blogic.ViewModels
-{
+{ 
     public class AdministrationViewModel
     {
         public List<Product> Products { get; set; } = [];
